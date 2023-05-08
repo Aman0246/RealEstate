f160032de816f23ac703162e80906c661ed35c9b
Aman Kashyap <amankashyap0246@gmail.com>
2023-05-08 10:45:03 +0530

    SliderSection

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Companies from './Component/Companies/Companies';
 import Header from './Component/Header/Header';
 import Hero from "./Component/Hero/Hero";
+import Swipe from './Component/Swiper_s/Swipe.js';
 function App() {
   return (
 <>
 <Header></Header>
 <Hero></Hero>
 <Companies></Companies>
+<Swipe></Swipe>
 </>
 
   );
